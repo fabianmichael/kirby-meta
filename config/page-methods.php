723598d329cd9bff67875824f939ac88a8b80619
Daan <idaany@gmail.com>
2023-01-11 22:17:09 +0100

    Page Method: isIndexedByMeta

--- config/page-methods.php
+++ config/page-methods.php
@@ -1,9 +1,14 @@
 <?php
 
 use FabianMichael\Meta\PageMeta;
+use FabianMichael\Meta\SiteMap;
 
 return [
     'meta' => function (?string $languageCode = null) {
         return PageMeta::of($this, $languageCode);
+    },
+
+    'isIndexedByMeta' => function(){
+        return Sitemap::isPageIndexible( $this );
     },
 ];
