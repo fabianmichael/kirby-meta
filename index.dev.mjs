35793526240a19a055abc916f86041aceaff1756
Fabian Michael <hallo@fabianmichael.de>
2024-03-22 17:59:08 +0100

    fix: update preview section for kirby 4

--- index.dev.mjs
+++
@@ -1,8 +0,0 @@
-try {
-  await import("http://[::1]:5177/panel/index.js");
-} catch (err) {
-  console.error(
-    "[kirbyup] Couldn't connect to the development server. Run `npm run serve` to start Vite or build the plugin with `npm run build` so Kirby uses the production version."
-  );
-  throw err;
-}
