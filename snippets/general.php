1aa427fa2a43920ab78bf9a54ee2c982ed74fac7
Tazio <tazio2003@gmail.com>
2024-10-17 19:42:37 +0200

    feat: Title smartypants

--- snippets/general.php
+++ snippets/general.php
@@ -1,10 +1,14 @@
-<title><?= $meta->title()->html() ?></title>
+<?php if (\FabianMichael\Meta\Helper::isSmartyPantsEnabled()): ?>
+    <title><?= $meta->title()->smartypants() ?></title>
+<?php else: ?>
+    <title><?= $meta->title()->html() ?></title>
+<?php endif ?>
 
 <?php if ($meta->description()->isNotEmpty()): ?>
     <?php if (\FabianMichael\Meta\Helper::isSmartyPantsEnabled()): ?>
         <meta name="description" content="<?= $meta->description()->smartypants() ?>">
     <?php else: ?>
-        <meta name="description" content="<?= html($meta->description()) ?>">
+        <meta name="description" content="<?= $meta->description()->html() ?>">
     <?php endif ?>
 <?php endif ?>
 
