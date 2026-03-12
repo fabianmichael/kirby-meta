e3b418ae985af3601ff367a9591bb7ac942ba69f
Fabian Michael <hallo@fabianmichael.de>
2026-03-12 05:37:31 +0100

    docs: add missing type hints and docblock comments

--- config/page-methods.php
+++ config/page-methods.php
@@ -1,35 +1,90 @@
 <?php
 
 use FabianMichael\Meta\PageMeta;
+use Kirby\Cms\Page;
 
 return [
-    'meta' => function (?string $languageCode = null) {
+    /**
+     * Get the page meta object for this page.
+     *
+     * @param ?string $languageCode The language code to get the meta for. If null, the current language will be used.
+     * @return PageMeta The page meta object.
+     */
+    'meta' => function (?string $languageCode = null): PageMeta {
         return PageMeta::of($this, $languageCode);
     },
 
+    /**
+     * Get the default meta values for this page.
+     *
+     * @param ?string $lang The language code to get the defaults for. If null, the current language will be used.
+     * @return array The default meta values.
+     */
     'metaDefaults' => function(?string $lang = null): array {
+        /** @var Page $this */
+
         return [];
     },
 
+    /**
+     * Get the override meta values for this page. These values can be use to
+     * force-override the default meta values and even user-defined values.
+     *
+     * @param ?string $lang The language code to get the overrides for. If null, the current language will be used.
+     * @return array The override meta values.
+     */
     'metaOverrides' => function(?string $lang = null): array {
+        /** @var Page $this */
+
         return [
             // 'robots.index' => false
         ];
     },
 
+    /**
+     * Check if the page is indexible, i.e. if the value of `robots.index` is true.
+     * You can customize the behavior of this method by adding it to your page
+     * models. This is mainly used for generating the sitemap and for the the
+     * panel view to show the indexibility status of the page.
+     *
+     * @return bool True if the page is indexible, false otherwise.
+     */
     'isIndexible' => function (): bool {
+        /** @var Page $this */
+
         return $this->meta()->robots('index');
     },
 
+    /**
+     * Get the status text for the indexibility of the page for the panel.
+     *
+     * @return string The status text.
+     */
     'isIndexibleStatusText' => function (): string {
+        /** @var Page $this */
+
         return r($this->isIndexible(), 'indexible', 'not indexible');
     },
 
+    /**
+     * Get the status icon for the indexibility of the page for the panel.
+     *
+     * @return string The status icon.
+     */
     'isIndexibleStatusIcon' => function (): string {
+        /** @var Page $this */
+
         return r($this->isIndexible(), 'meta-eye', 'meta-eye-off');
     },
 
+    /**
+     * Get the status theme for the indexibility of the page for the panel.
+     *
+     * @return string The status theme.
+     */
     'isIndexibleTheme' => function (): string {
+        /** @var Page $this */
+
         return r($this->isIndexible(), 'info-icon', 'warning-icon');
     },
 ];
